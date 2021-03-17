89a571358379e6ad7338d068bd89c9933c08035e
V S <66644986+Serendipity11@users.noreply.github.com>
2021-03-16 23:01:41 -0400

    Created default functions index.js

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ displayTime();
 let apiKey = "9f7b34ce1d4954adbe45333199ef6c7a";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
 let units = "imperial";
+
 function defaultWeather() {
   axios
     .get(`${apiUrl}q=new york&appid=${apiKey}&units=${units}`)
@@ -37,6 +38,85 @@ function getDefaultWeather(res) {
   console.log(res);
   sky.innerHTML = res.data.weather[0].main;
   let iconcode = res.data.weather[0].icon;
+  locationIcon.src = "http://openweathermap.org/img/w/" + iconcode + ".png";
+  temperature.innerHTML = Math.round(res.data.main.temp) + "°F";
+  humidity.innerHTML = Math.round(res.data.main.humidity);
+  pressure.innerHTML = Math.round(res.data.main.pressure);
+  wind.innerHTML = Math.round(res.data.wind.speed);
+  defaultForecast();
+}
+function defaultForecast() {
+  let apiUrl = "https://api.openweathermap.org/data/2.5/forecast?";
+
+  axios
+    .get(`${apiUrl}q=new york&appid=${apiKey}&units=${units}`)
+    .then(displayDefaultForecast);
+}
+function displayDefaultForecast(res) {
+  console.log(res);
+  let firstDate = new Date(res.data.list[4].dt * 1000);
+  let firstDay = firstDate.getDay();
+  console.log(firstDay, res.data.list[4].dt_txt);
+
+  let secondDate = new Date(res.data.list[12].dt * 1000);
+  let secondDay = secondDate.getDay();
+  console.log(secondDay, res.data.list[12].dt_txt);
+
+  let thirdDate = new Date(res.data.list[20].dt * 1000);
+  let thirdDay = thirdDate.getDay();
+  console.log(thirdDay, res.data.list[20].dt_txt);
+
+  let forthDate = new Date(res.data.list[28].dt * 1000);
+  let forthDay = forthDate.getDay();
+  console.log(forthDay, res.data.list[28].dt_txt);
+
+  let fifthDate = new Date(res.data.list[36].dt * 1000);
+  let fifthDay = fifthDate.getDay();
+  console.log(fifthDay, res.data.list[36].dt_txt);
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tueday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Satursday",
+  ];
+
+  dayOne.innerHTML = days[firstDay];
+  dayTwo.innerHTML = days[secondDay];
+  dayThree.innerHTML = days[thirdDay];
+  dayFour.innerHTML = days[forthDay];
+  dayFive.innerHTML = days[fifthDay];
+
+  oneTemp.innerHTML = Math.round(res.data.list[4].main.temp);
+  twoTemp.innerHTML = Math.round(res.data.list[12].main.temp);
+  threeTemp.innerHTML = Math.round(res.data.list[20].main.temp);
+  fourTemp.innerHTML = Math.round(res.data.list[28].main.temp);
+  fiveTemp.innerHTML = Math.round(res.data.list[36].main.temp);
+
+  oneIcon.src =
+    "http://openweathermap.org/img/w/" +
+    res.data.list[4].weather[0].icon +
+    ".png";
+
+  twoIcon.src =
+    "http://openweathermap.org/img/w/" +
+    res.data.list[12].weather[0].icon +
+    ".png";
+  threeIcon.src =
+    "http://openweathermap.org/img/w/" +
+    res.data.list[20].weather[0].icon +
+    ".png";
+  fourIcon.src =
+    "http://openweathermap.org/img/w/" +
+    res.data.list[28].weather[0].icon +
+    ".png";
+  fiveIcon.src =
+    "http://openweathermap.org/img/w/" +
+    res.data.list[36].weather[0].icon +
+    ".png";
 }
 
 defaultWeather();
